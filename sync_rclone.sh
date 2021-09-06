ad712eedd7ac7e22db5c727741a2707ce674add1
mrdotx <klassiker@gmx.de>
2021-09-06 18:08:41 +0200

    write usage to file added

--- sync_rclone.sh
+++ sync_rclone.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/rclone/sync_rclone.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/rclone
-# date:   2021-01-15T13:53:52+0100
+# date:   2021-09-06T17:54:25+0200
 
 # color variables
 green=$(tput setaf 2)
@@ -16,10 +16,10 @@ help="$script [-h/--help] -- script to copy/sync from/to cloud with rclone
     $script [-check/-copy/-sync_to/-sync_from]
 
   Setting:
-      -check     = check
-      -copy      = copy
-      -sync_to   = sync to destination
-      -sync_from = sync from destination
+      -check     = check differences between local and cloud
+      -copy      = copy from/to cloud
+      -sync_to   = sync to cloud
+      -sync_from = sync from cloud
 
   Example:
     $script -check
@@ -27,82 +27,55 @@ help="$script [-h/--help] -- script to copy/sync from/to cloud with rclone
     $script -sync_to
     $script -sync_from"
 
-rclone_directory="$HOME/.local/share/cloud"
+rclone_dir="$HOME/.local/share/cloud"
 rclone_config="
-web.de;       $rclone_directory/webde/;       webde:/;       $rclone_directory/webde/.filter
-GMX;          $rclone_directory/gmx/;         gmx:/;         $rclone_directory/gmx/.filter
-Google Drive; $rclone_directory/googledrive/; googledrive:/; $rclone_directory/googledrive/.filter
-OneDrive;     $rclone_directory/onedrive/;    onedrive:/;    $rclone_directory/onedrive/.filter
-Dropbox;      $rclone_directory/dropbox/;     dropbox:/;     $rclone_directory/dropbox/.filter
+      web.de; $rclone_dir/webde/;       webde:/;        8,0G
+         GMX; $rclone_dir/gmx/;         gmx:/;          8,0G
+Google Drive; $rclone_dir/googledrive/; googledrive:/; 17,0G
+    OneDrive; $rclone_dir/onedrive/;    onedrive:/;     5,0G
+     Dropbox; $rclone_dir/dropbox/;     dropbox:/;      3,25G
 "
 
-rclone_vars() {
-    title=$(printf "%s" "$1" \
-        | cut -d ";" -f1 \
-        | tr -d ' ' \
-    )
-    source_directory=$(printf "%s" "$1" \
-        | cut -d ";" -f2 \
-        | tr -d ' ' \
-    )
-    destination_directory=$(printf "%s" "$1" \
-        | cut -d ";" -f3 \
-        | tr -d ' ' \
-    )
-    filter_file=$(printf "%s" "$1" \
-        | cut -d ";" -f4 \
-        | tr -d ' ' \
-    )
+get_config_value() {
+    printf "%s" "$1" \
+        | cut -d ";" -f"$2" \
+        | tr -d ' '
 }
 
-rclone_check() {
-    printf "[%s%s%s] <-> %s%s%s\n" "$green" "$1" "$reset" "$blue" "$2" "$reset"
-    rclone check -l -P "$2" "$3" --filter-from="$4"
+clone() {
+    printf "[%s%s%s] $2 %s%s%s\n" "$green" "$4" "$reset" "$blue" "$5" "$reset"
+    rclone "$1" -l -P "$4" "$5" --filter-from="$6.filter"
     printf "\n"
+    printf "%s/%s\n" "$(du -sh "$6" | cut -d'	' -f1)" "$7" > "$6.usage"
 }
 
-rclone_copy() {
-    printf "[%s%s%s] <- %s%s%s\n" "$green" "$1" "$reset" "$blue" "$2" "$reset"
-    rclone copy -l -P "$2" "$3" --filter-from="$4"
-    printf "[%s%s%s] -> %s%s%s\n" "$green" "$1" "$reset" "$blue" "$2" "$reset"
-    rclone copy -l -P "$3" "$2" --filter-from="$4"
-    printf "\n"
-}
-
-rclone_sync_to() {
-    printf "[%s%s%s] <- %s%s%s\n" "$green" "$1" "$reset" "$blue" "$2" "$reset"
-    rclone sync -l -P "$2" "$3" --filter-from="$4"
-    printf "\n"
-}
-
-rclone_sync_from() {
-    printf "[%s%s%s] -> %s%s%s\n" "$green" "$1" "$reset" "$blue" "$2" "$reset"
-    rclone sync -l -P "$3" "$2" --filter-from="$4"
-    printf "\n"
-}
-
-rclone_execute() {
+execute() {
     printf "%s\n" "$rclone_config" | {
         while IFS= read -r line; do
             [ -n "$line" ] \
-                && rclone_vars "$line" \
-                && $1 "$title" "$source_directory" "$destination_directory" "$filter_file"
+                && clone "$1" "$3" \
+                    "$(get_config_value "$line" "1")" \
+                    "$(get_config_value "$line" "$2")" \
+                    "$(get_config_value "$line" "$4")" \
+                    "$(get_config_value "$line" "2")" \
+                    "$(get_config_value "$line" "4")"
         done
     }
 }
 
 case "$1" in
     -check)
-        rclone_execute "rclone_check"
+        execute "check" "2" "<->" "3"
         ;;
     -copy)
-        rclone_execute "rclone_copy"
+        execute "copy" "2" "->" "3"
+        execute "copy" "3" "->" "2"
         ;;
     -sync_to)
-        rclone_execute "rclone_sync_to"
+        execute "sync" "2" "->" "3"
         ;;
     -sync_from)
-        rclone_execute "rclone_sync_from"
+        execute "sync" "3" "->" "2"
         ;;
     *)
         printf "%s\n" "$help"
